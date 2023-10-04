1f6b99302d7ae8453e064be14d52be17d5ae3985
christine <107821719+chrixti@users.noreply.github.com>
2023-10-04 13:09:04 -0400

    edits to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../style/about.css';
-import img1 from '../assets/projectplaceholder.png';
+import img1 from '../assets/photo.jpeg';
 
 
 const About = () => {
@@ -18,7 +18,8 @@ const About = () => {
               <div className='about-text-block'> Currently...learning rock climbing
               </div>
             </div>
-            <p className='about-text-p'>Hello there, my name is Christine and I'm from New York City! I'm an undergraduate student at Barnard College of Columbia University in the City of New York where I major in Computer Science with a minor in Psychology.‍<br></br><br></br>During my free time, I immerse myself in a world of my imagination by creating art. If I'm not drawing then I'm online playing video games with friends or trying to learn something new! </p>
+            <p className='about-text-p'>Hello there, my name is Christine and I'm from New York City! I'm an undergraduate student at Barnard College of Columbia University in the City of New York where I major in Computer Science with a minor in Psychology.‍<br></br>
+            <br></br>During my free time, I immerse myself in a world of my imagination by creating art. If I'm not drawing then I'm online playing video games with friends or trying to learn something new! </p>
           </div>
         </div>
       </div>
@@ -39,12 +40,12 @@ const About = () => {
               <div className='text-block-6'>HTML & CSS</div>
               <div className='text-block-6'>Javascript</div>
               <div className='text-block-6'>React.js</div>
-              <div className='text-block-6'>Swift</div>
             </div>
             <div className='skill-group'>
               <div className='title-small'>Languages</div>
               <div className='text-block-6'>English (fluent)</div>
               <div className='text-block-6'>Mandarin (fluent)</div>
+              <div className='text-block-6'>Japanese (conversational)</div>
             </div>
             <div className='skill-group'>
               <div className='title-small'>Other</div>
